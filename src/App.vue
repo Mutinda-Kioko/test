34de1dc55f5112345d892f4be75f123c921e3583
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-02-21 18:27:09 +0300

    Added the fetchCoins to src and imported in App

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,77 @@
-<script setup>
+<script>
   import { getPrice } from './fetchCoins';
+  import SwapForm from './SwapForm.vue';
+  let value
+  let coins = []
   const addresses = [
     { symbol:"BTC",
+      img:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/BTC_Logo.svg/2000px-BTC_Logo.svg.png",
       addr:"0xECe365B379E1dD183B20fc5f022230C044d51404"},
     { symbol:"ETH",
+      img:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/628px-Ethereum_logo_2014.svg.png",
       addr:"0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"},
     { symbol:"USDC",
+      img:"https://seeklogo.com/images/U/usd-coin-usdc-logo-CB4C5B1C51-seeklogo.com.png",
       addr:"0xa24de01df22b63d23Ebc1882a5E3d4ec0d907bFB"},
     { symbol:"BNB",
+      img:"https://seeklogo.com/images/B/binance-coin-bnb-logo-97F9D55608-seeklogo.com.png",
       addr:"0xcf0f51ca2cDAecb464eeE4227f5295F2384F84ED"},
     { symbol:"DAI",
-      addr:"0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF"}]
-  async function fetchCoins(){
-    for(let i=0;i<addresses.length; i++){
-      const value = await getPrice(addresses[i].addr);
-      console.log(addresses[i].symbol, value);
-    }
-
-  }
-  fetchCoins();
+      img:"https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png",
+      addr:"0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF"}
+]
+export default {
+    data() {
+        return {
+            coins,
+            loading: true,
+            display: false,
+        };
+    },
+    mounted() {
+        async function fetchCoins() {
+            for (let i = 0; i < addresses.length; i++) {
+                value = await getPrice(addresses[i].addr);
+                coins.push({
+                    symbol: addresses[i].symbol,
+                    price: value,
+                    imgURL: addresses[i].img
+                });
+            }
+            console.log("coins : ", coins);
+        }
+        ;
+        fetchCoins().then(() => this.loading = false);
+    },
+    methods: {
+        proceedToSwap: function (coin) {
+           this.display = true;
+        }
+    },
+    components: { SwapForm }
+};
 </script>
 
 <template>
   <div class="card-container">
     <div class="card">
-      <div class="listComponent">
+      <div v-if="loading">Loading...</div>
+      <div v-if="!loading">
+      <div class="listComponent" @click="proceedToSwap"  v-for="coin in coins" :key="coin.symbol">
       <div class="box">
         <article class="media">
           <div class="media-left">
             <figure class="image">
-              <p>Bitcoin: </p>
+              <img :src="coin.imgURL" :alt="coin.symbol">
             </figure>
           </div>
           <div class="media-content">
             <div class="content">
-              <p><strong>123456</strong> </p>
+              <p>{{ coin.symbol }} : <strong>{{ coin.price }}</strong></p> 
             </div>
           </div>
         </article>
+      </div>
       </div>
     </div>
     </div>
@@ -48,7 +82,6 @@
 @import './assets/base.css';
 
 #app {
-  max-width: 1280px;
   font-weight: normal;
   display: flex;
   justify-content: center;
@@ -73,7 +106,7 @@
   background-color: #fff;
   color: #373737;
   box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-  padding: 30px;
+  padding: 10px;
   margin-top: 20px;
   }
 .listComponent:hover{
@@ -88,8 +121,9 @@
     display: flex;
   }
   img {
-    width: 64px;
+    width: 30px;
     margin-right: 15px;
+    margin-top: 15px;
   }
   small {
     font-size: 14px;
